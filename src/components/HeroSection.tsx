833c02e1cb212dc2e11e9018c5669ae58c6040fe
apurva470 <apurvasolanki70@gmail.com>
2024-09-14 20:39:14 +0530

    resume button added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Spotlight } from '@/components/ui/Spotlight'
 import { TypewriterEffectSmooth } from './ui/typewriter-effect';
 import { TextGenerateEffect } from './ui/text-generate-effect';
+import Link from 'next/link';
 
 function HeroSection() {
     const wordsForTextGenerateEffect="Code infrequently, think constantly: My approch to problem solving."
@@ -29,8 +30,6 @@ function HeroSection() {
                     fill="white"
                 />
                 <div className='p-4 relative z-10 w-full text-center'>
-                    
-                    
                     <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>
                         <div className="flex flex-col items-center justify-center">
                             <TypewriterEffectSmooth words={wordsForTypewriter} />
@@ -38,6 +37,13 @@ function HeroSection() {
                     </h1>
                     <div className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>
                         <TextGenerateEffect words={wordsForTextGenerateEffect} />
+                    </div>
+                    <div className='mt-4'>
+                        <Link href="https://drive.google.com/file/d/1_tuEc06mxGMYGanQS4hOHbZuvINw3M9J/view?usp=sharing" target="_blank">
+                            <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                                Resume
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
