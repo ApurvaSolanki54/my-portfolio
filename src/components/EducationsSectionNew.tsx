9f43197126c6b4c2d23ec33269a762e4e1266c6d
apurva470 <apurvasolanki70@gmail.com>
2025-02-19 20:51:06 +0530

    change background of education

--- src/components/EducationsSectionNew.tsx
+++ src/components/EducationsSectionNew.tsx
@@ -3,9 +3,9 @@ import { GlowingEffect } from "@/components/ui/glowing-effect";
 
 export function EducationsSectionNew() {
     return (<>
-        <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-center">
+        <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative items-center justify-center">
             <div className='mt-8 text-center'>
-                <p className='text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl pt-10'>EDUCATIONS</p>
+                <p className='text-3xl leading-8 font-extrabold text-white sm:text-4xl pt-10'>EDUCATIONS</p>
             </div>
             {/* Radial gradient for the container to give a faded look */}
             <div className="mt-8 absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
