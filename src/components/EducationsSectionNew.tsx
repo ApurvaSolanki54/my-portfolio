6c47cb88038259c68ecf1d2dc67b00ca46ddf437
apurva470 <apurvasolanki70@gmail.com>
2025-02-19 10:23:12 +0530

    margin added in education header

--- src/components/EducationsSectionNew.tsx
+++ src/components/EducationsSectionNew.tsx
@@ -3,12 +3,12 @@ import { GlowingEffect } from "@/components/ui/glowing-effect";
 
 export function EducationsSectionNew() {
     return (<>
-        <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-center">
-            <div className='text-center'>
-                <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>EDUCATIONS</p>
+        <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-center">
+            <div className='mt-8 text-center'>
+                <p className='text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl pt-10'>EDUCATIONS</p>
             </div>
             {/* Radial gradient for the container to give a faded look */}
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+            <div className="mt-8 absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 ">
                 <ul className="grid grid-cols-1 grid-rows-none gap-4 md:grid-cols-12 md:grid-rows-3 lg:gap-4 xl:max-h-[34rem] xl:grid-rows-2 ">
 
